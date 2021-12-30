9fa90a70fa25fa9f48f4e692a1d3527c199c7ddd
알류디 <phk08060@naver.com>
2021-12-30 11:11:03 +0900

    ex3-2 : movie app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import {useState, useEffect} from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+
   useEffect( () => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-    .then((response) => response.json())
-    .then((json) => {
-      setMovies(json.data.movies);
-      setLoading(false);
-      console.log(movies);
-    });
+    getMovies();
   }, [])
-  
+  console.log(movies);
   return (
     <div>
       
