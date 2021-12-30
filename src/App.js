4411654cd878754bdd8c92b4b016f290ffc12137
알류디 <phk08060@naver.com>
2021-12-30 11:46:03 +0900

    ex3-3 : movie app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async() => {
-    const response = await fetch(
+    const json = await (
+      await fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
-    const json = await response.json();
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   }
@@ -15,10 +16,25 @@ function App() {
   useEffect( () => {
     getMovies();
   }, [])
-  console.log(movies);
+  // console.log(movies);
   return (
     <div>
-      
+      {loading? (
+        <h1>Loading...</h1>
+      ) : (
+        movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} />
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g, index) => (
+                <li key={index}>{g}</li>
+              ))}
+            </ul>
+          </div>
+        ))
+      )}
     </div>
   );
 }
